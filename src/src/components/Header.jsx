67bb30f8480a08eef19eb9b919c06a023ac0e710
dreddcats <jeremy.edwards93@outlook.com>
2025-08-08 03:57:07 -0500

    Add files via upload

---
+++ src/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Header = () => (
+  <header className="header">
+    <h1>GamerDNA</h1>
+    <p>Discover the games that match your DNA</p>
+  </header>
+);
+
+export default Header;
